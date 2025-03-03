0b9097b0f2e5a7eef8b1602eae2a9f306597b138
masonnguyen <100501225+jasoncuong@users.noreply.github.com>
2025-03-03 19:21:32 +0700

    Cart Reducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
 import productsReducer from "./slice/productsSlice";
+import cartReducer from "./slice/cartSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productsReducer,
+    cart: cartReducer,
   },
 });
 
